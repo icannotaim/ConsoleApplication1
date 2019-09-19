211137dc97f034448708e72afe711c93963e658e
icannotaim_curtis <icannotaim_curtis@outlook.com>
2019-09-19 11:12:38 -0400

    initial commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// is this a valid comment?
 namespace ConsoleApplication1
 {
     class Program
